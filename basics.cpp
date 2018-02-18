56b9cd98d6e9956f3156b6a946145a3a82e24171
aakashvarma18 <aakashvarma18@gmail.com>
2018-02-19 03:36:55 +0530

    Doubt in input() function

--- basics.cpp
+++ basics.cpp
@@ -9,23 +9,23 @@ int max(int a, int b){
 		return b;
 	}
 };
-//void input(){
-//	int n1, n2;
-//	cout<<"Enter 1st number: ";
-//	cin>>n1;
-//	cout<<"Enter 2nd number: ";
-//	cin>>n2;
-//};
+int input(){
+	int num1, num2;
+	cout<<"Enter 1st number: ";
+	cin>>num1;
+	cout<<"Enter 2nd number: ";
+	cin>>num2;
+};
 int display(int c){
 	cout<<"The maximum of the two inputed numbers is: "<<c;
 };
 int main(){
 	int n1, n2;
-//	input();
-	cout<<"Enter 1st number: ";
-	cin>>n1;
-	cout<<"Enter 2nd number: ";
-	cin>>n2;
+	input();
+//	cout<<"Enter 1st number: ";
+//	cin>>n1;
+//	cout<<"Enter 2nd number: ";
+//	cin>>n2;
 	int ret = max(n1, n2);
 	display(ret);
 }
