e60b9a40d382e01bf62296c107c67770c890c6b9
aakashvarma18 <aakashvarma18@gmail.com>
2018-02-20 03:45:12 +0530

    Pointers with arrays added

--- pointers.cpp
+++ pointers.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 
 int main(){
-	int var = 20;
-	int *ip = &var;
-	
-	cout<<&var<<endl;
-	cout<<*ip;
+	int i, a[] = {10, 20, 30, 40, 50};
+	int *p;
+	p = a;
+//	cout<<*p<<endl<<&p;
+	for(i = 0; i<=(sizeof(a)/sizeof(*a)); i++){
+		
+		cout << "The value of a[" << i << "]" << p << endl;
+		p++;
+	}
 }
+
