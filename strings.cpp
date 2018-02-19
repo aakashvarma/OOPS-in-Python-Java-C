a6859a4e22ca628e9331e2541bf095e514b6c471
aakashvarma18 <aakashvarma18@gmail.com>
2018-02-19 22:16:46 +0530

    String as array added

--- strings.cpp
+++ strings.cpp
@@ -2,29 +2,11 @@
 #include <string>
 using namespace std;
 
-string concat(string a, string b);
-string input();
-string display();
-
 int main(){
-    string str1, str2, str3;
-	input();
-	concat(str1, str2);
-	display();	
-}
-string concat(string str1, string str2){
-	string str3 = str1 + str2;
-	return str3;
-}
-string input(){
 	string str1, str2;
-	cout<<"Enter string 1:";
-	cin>>str1;
-	cout<<"Enter string 2:";
-	cin>> str2;
-}
-string display(){
-	string str3, str4;
-	cout<<"The concatened output is: "<<str3;
-	cout<<"The copied output id: "<<str4;
+	str1 = "Aakash";
+	str2 = "Varma";
+	string str3 = str1 + str2;
+	cout << str3 << endl;
+	cout<<str3[1];
 }
