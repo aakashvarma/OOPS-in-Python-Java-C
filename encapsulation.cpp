8fc6af336c02e1418f06298cd910625e253c4ab1
aakashvarma18 <aakashvarma18@gmail.com>
2018-02-24 03:35:00 +0530

    Encapsulation not completed

---
+++ encapsulation.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+class size{
+	public:
+		int width;
+		int height;
+		int *dimensions();
+		int *p;
+		 
+	private:
+		int total;
+};
+int size:: *dimension(){
+	int w[];
+	cout<<"Enter the width";
+	cin>>w[0];
+	cout<<endl;
+	cout<<"Enter the height";
+	cin>>w[1];
+	cout<<endl;
+	return w;
+}
+void display(){
+	cout<<
+}
+
+int main(){
+	
+}
