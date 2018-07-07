dabb6df353b7581ced1e0975faf0ecdd0b8b08d5
aakashvarma18 <aakashvarma18@gmail.com>
2018-07-07 23:07:14 +0530

    array pointers added

--- basics.cpp
+++ basics.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	int n;
-	cout << &n <<endl;
+int main()
+{
 	
-	int *n_pointer;
-	n_pointer = &n;
-	cout << n_pointer <<endl;
+	int numbers[3];
+	int *p;
+	p = numbers;
+	*p = 10;
+	p++;
+	*p = 20;
+	p = &numbers[2];
+	*p = 30;
+	
+	for (int n = 0; n < 3; n++){
+		cout << numbers[n] << ",";
+	}
+	return 0;
 	
 } 
